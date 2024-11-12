da2834fc1a2cdbb0767f23558f9257f9522c1523
a.sedov <alexejsedow@gmail.com>
2024-11-12 19:11:32 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа 2-6");
+        System.out.println();
+        System.out.println("Задание 1");
+        List nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
     }
 }
